bc301de3d6ec09e8998e0bc18bce1ef139656a14
muzi-tano <muzitanoamatheus3@gmail.com>
2026-04-15 08:25:30 -0300

    Ajustando o Map

---
+++ prisma/migrations/20260415103124_mapping_table_names/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - Added the required column `content` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userId` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `post` ADD COLUMN `content` VARCHAR(191) NOT NULL,
+    ADD COLUMN `userId` INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
