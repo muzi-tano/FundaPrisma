0bd681bf2f657586126cc56cb8ead36a908d3318
muzi-tano <muzitanoamatheus3@gmail.com>
2026-04-22 08:42:11 -0300

    Iniciando o CRUD

---
+++ services/user.ts
@@ -0,0 +1,14 @@
+import { Prisma } from '@prisma/client'
+import { prisma } from "../src/libs/prisma"
+
+export const createUser = async (name: string , email: string) => 
+{
+    const user = await prisma.user.create ({
+ data: {
+    name,
+    email
+ }
+
+    })
+    return user
+}
--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,18 @@
 import { Router } from 'express';
+import { prisma } from '../libs/prisma';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
 });
+
+mainRouter.post ('/user', async (req, res) => {
+
+const user = await createUser(
+ 
+    'John Doe',
+     'john.doe@example.com'
+
+)
+res.json(user) })
