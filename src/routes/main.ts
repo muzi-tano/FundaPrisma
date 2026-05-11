b6a610bfb3a6fc3dab56ea0bcdcee82525da5937
muzi-tano <muzitanoamatheus3@gmail.com>
2026-05-11 07:38:17 -0300

    atualizando código

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,24 +1,21 @@
 import { Router } from 'express';
 import { prisma } from '../libs/prisma'
-import { createUser } from 'services/user.ts';
+import { createUser } from '../services/user';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
-  res.json({ pong: true });
+    res.json({ pong: true });
 });
 
 mainRouter.post('/user', async (req, res) => {
-  try {
     const user = await createUser({
-      name: 'John Doe',
-      email: 'john.doe@exemple.com',
+        name: 'John Doe',
+        email: 'john.doe@exemple.com'
     });
-
-
-    res.status(201).json(user);
-  } catch (error) {
-  
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
+    if (user) {
+        res.status(201).json({ user });
+    } else {
+        res.status(400).json({ error: 'Email already exists' });
+    }
+})
